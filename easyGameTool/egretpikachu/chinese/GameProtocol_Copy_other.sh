b43aa7a2f2432ccf275a3e33d0fbc93cb14efc39
刘江 <1433727556@qq.com>
2018-11-26 15:23:47 +0800

    add flask test

--- easyGameTool/egretpikachu/chinese/GameProtocol_Copy_other.sh
+++ easyGameTool/egretpikachu/chinese/GameProtocol_Copy_other.sh
@@ -1,5 +1,5 @@
 
-svnpath="/Users/admin/Downloads/TsOut(9)/"
+svnpath="/Users/admin/Downloads/TsOut(12)/"
 protocolOutPath="/Users/admin/Documents/ljworkspace/local/egret/SmartPikachu/SmartPikachuGit/SmartPikachu/SmartpikachuGame/libs/ezModules/gameProtocol"
 resourcePath="/Users/admin/Documents/ljworkspace/local/egret/SmartPikachu/SmartPikachuGit/SmartPikachu/SmartpikachuGame/resource/data/"
 
