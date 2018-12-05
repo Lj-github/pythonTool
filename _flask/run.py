bb4785bd3a71b048d8c3fa366c000c5606cafd9e
liujiang <1433727556@qq.com>
2018-12-05 12:07:36 +0800

    添加注释

--- _flask/run.py
+++ _flask/run.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2018/11/26 下午3:16
 from _flask import app
-app.run(host="0.0.0.0", debug=True)
+app.run(host="0.0.0.0", debug=True,port=9613)
 
+#使用 localhost  或者 ip
