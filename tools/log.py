f9366bd310811a8f54eccb3bd5897f4c7988102b
刘江 <1433727556@qq.com>
2018-12-12 16:51:25 +0800

    add log tool

---
+++ tools/log.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/12 下午3:24
+
+
+import logging
+
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='./logger.log',
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+print = logging.info  # python3 print 变为函数 可以运行 2 不行
+
+
+
