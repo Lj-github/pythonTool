ad8dff56fcdbc74d803ef211e42c1a16372f74a3
刘江 <1433727556@qq.com>
2018-12-20 19:44:33 +0800

    add tools

---
+++ easyGameTool/foreignTools/cocosPikachuTools/english/copyWinterImg_english.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/19 下午6:01
+
+
+# 冬季皮肤替换
+import easyGameTool.foreignTools.cocosPikachuTools.ExcelTools as et
+import os
+
+isShenDan = True
+
+shendanFile = '/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/动画/替换怪物/节日精灵/圣诞'
+
+baseFile =  "/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/动画/替换怪物"
+
+
+projectFile = '/Users/admin/Documents/ljworkspace/local/cocos/project/pikachu_englishGit/pikachu_english/app/static/res/unit'
+
+if __name__ == '__main__':
+
+    allImgFile = et.getFileOnlyName(shendanFile,["png"])
+
+    for f in allImgFile:
+        fp,fn = os.path.split(f)
+        if not isShenDan:
+            shendanFile = baseFile
+
+        et.copyfile(shendanFile+ "/" + fn,projectFile+ "/" + fn)
+
+
+    pass
