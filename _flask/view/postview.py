f88d37396a7b3a90a58dce5288ef8468386090e5
刘江 <1433727556@qq.com>
2018-12-24 12:27:59 +0800

    flask save txt

--- _flask/view/postview.py
+++ _flask/view/postview.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2018/12/24 上午11:06
-from flask import render_template, request,make_response
+from flask import render_template, request, make_response
 from _flask import app
 from flask import jsonify
 import json
@@ -8,8 +8,8 @@ import json
 
 @app.route('/post/', methods=['POST'])
 def postTest():
-    username = request.values['username'] #json['username']
-    content = request.values['content'] #.json['']
+    username = request.values['username']  # json['username']
+    content = request.values['content']  # .json['']
     print("username " + username)
     print("content " + content)
     loader = {}
@@ -21,3 +21,15 @@ def postTest():
 def mydict():
     d = {'name': 'xmr', 'age': 18}
     return jsonify(d)
+
+
+@app.route('/postSaveTxt/', methods=['POST'])
+def postSaveTxt():
+    username = request.values['username']
+    content = request.values['content']
+    print('username  ' + username)
+    with open("static/txt/" + username, 'w') as f:
+        f.write(content)
+    loader = {}
+    loader['dd'] = 'success'
+    return json.dumps(loader), 200, {'Content-Type': 'application/json; charset=utf-8'}
