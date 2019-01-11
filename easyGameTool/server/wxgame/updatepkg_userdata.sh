fde43cac5aa471693071a071f331a2b2d1c2a68b
刘江 <1433727556@qq.com>
2019-01-11 19:56:42 +0800

    add tools

---
+++ easyGameTool/server/wxgame/updatepkg_userdata.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+mysql -h127.0.0.1 -uroot -p123456 pkc_userdata</Users/admin/Documents/ljworkspace/server/pikachuserver_wx/pikachu/db/sql/userdata_tables.sql
+echo 'update success!'
