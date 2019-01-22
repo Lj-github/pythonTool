d8e5e659ea6c01a7bbf3433b9cb4753c8dd174d0
刘江 <1433727556@qq.com>
2019-01-22 15:16:04 +0800

    change tool

--- easyGameTool/server/mysql/updatepkg_webdata.sh
+++ easyGameTool/server/mysql/updatepkg_webdata.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/playerloginzhenghe_t.sql
-mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/playerorderzhenghe_t.sql
-mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/playerorderzhenghe.sql
-mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/playerloginzhenghe.sql
+mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/daily_player_2019_01_18_t.sql
+mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/daily_player_2019_01_18.sql
+#mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/playerorderzhenghe_t.sql
+#mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/playerloginzhenghe.sql
 echo 'update success!'
