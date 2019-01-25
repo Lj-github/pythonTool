02c3b5d9a9c80288a2901d0bb8dfc0500411e129
刘江 <1433727556@qq.com>
2019-01-25 19:40:24 +0800

    add tools

--- easyGameTool/server/mysql/updatepkg_webdata.sh
+++ easyGameTool/server/mysql/updatepkg_webdata.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/daily_player_2019_01_18_t.sql
 #mysql -h127.0.0.1 -uroot -p123456 pkc_webdata</Users/admin/Desktop/daily_player_2019_01_18.sql
-mysql -h127.0.0.1 -uroot -proot pkc_webdata</Users/admin/Desktop/daily_player_2018_11_23_t.sql
-mysql -h127.0.0.1 -uroot -proot pkc_webdata</Users/admin/Desktop/daily_player_2018_11_23.sql
+#mysql -h127.0.0.1 -uroot -proot pkc_webdata</Users/admin/Desktop/daily_player_2018_11_23_t.sql
+#mysql -h127.0.0.1 -uroot -proot pkc_webdata</Users/admin/Desktop/daily_player_2018_11_23.sql
 
-#mysql -h127.0.0.1 -uroot -proot pkc_webdata</Users/admin/Documents/ljworkspace/server/gmWeb/sgWebServices/sqls/alter_20190118.sql
+#mysql -h127.0.0.1 -uroot -proot pkc_webdata</Users/admin/Documents/ljworkspace/server/gmWeb/sgWebServices/sqls/pkc_webdata_tables.sql
+mysql -h127.0.0.1 -uroot -proot pkc_webdata</Users/admin/Documents/ljworkspace/local/python/pythonTool/easyGameTool/server/mysql/userwebdata.sql
 echo 'update success!'
