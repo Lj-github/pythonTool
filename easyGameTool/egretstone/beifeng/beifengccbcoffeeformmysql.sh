4eee96193cc889576b5567ec3e643ae68e4ae684
刘江 <1433727556@qq.com>
2019-02-13 17:01:17 +0800

    change tools

--- easyGameTool/egretstone/beifeng/beifengccbcoffeeformmysql.sh
+++ easyGameTool/egretstone/beifeng/beifengccbcoffeeformmysql.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ruby /Users/admin/Documents/ljworkspace/local/ruby/rubyTest/easygameTools/pikachu/beifeng/mysql_to_xls.rb
+ruby /Users/admin/Documents/ljworkspace/local/ruby/rubyTest/easygameTools/stoneAge/beifeng/mysql_to_xls.rb
