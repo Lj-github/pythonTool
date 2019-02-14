779b0684f386701c89453a35e0895d9913474747
刘江 <1433727556@qq.com>
2019-02-14 20:58:43 +0800

    添加注释

--- easyGameTool/foreignTools/cocosPikachuTools/russion/CCBCoffeeCopy_russion_update.sh
+++ easyGameTool/foreignTools/cocosPikachuTools/russion/CCBCoffeeCopy_russion_update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 #直接 从 英文版里面 覆盖 需要 提前 同步 至 英文版
 
+#是否更新coffee
+isUpdateCoffee=true
+
 
 rm -rf /Users/admin/Documents/ljworkspace/local/cocos/project/pikachu_russion_update/pikachu_english/tools/pikachuCCB/ccb/
 mkdir -p /Users/admin/Documents/ljworkspace/local/cocos/project/pikachu_russion_update/pikachu_english/tools/pikachuCCB/ccb/
@@ -8,7 +11,6 @@ mkdir -p /Users/admin/Documents/ljworkspace/local/cocos/project/pikachu_russion_
 
 engLishPrp=/Users/admin/Documents/ljworkspace/local/cocos/project/pikachu_englishGit/pikachu_english/
 appPath=/Users/admin/Documents/ljworkspace/local/cocos/project/pikachu_russion_update/pikachu_english/
-isUpdateCoffee=true
 if $isUpdateCoffee = true ;then
     rm -rf /Users/admin/Documents/ljworkspace/local/cocos/project/pikachu_russion_update/pikachu_english/app/static/coffee/
     mkdir -p /Users/admin/Documents/ljworkspace/local/cocos/project/pikachu_russion_update/pikachu_english/app/static/coffee/
