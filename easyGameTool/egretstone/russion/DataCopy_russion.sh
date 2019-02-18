3090ed48ab882e8ed0a23d9e90329c3acf373ecd
刘江 <1433727556@qq.com>
2019-02-18 17:14:52 +0800

    d3js add

--- easyGameTool/egretstone/russion/DataCopy_russion.sh
+++ easyGameTool/egretstone/russion/DataCopy_russion.sh
@@ -8,7 +8,7 @@ mkdir -p /Users/admin/Documents/ljworkspace/local/egret/ProStoneAge/ProStoneAge/
 find /Users/admin/Documents/ljworkspace/local/egret/design/stone_age/tools/exceltojson/json -iname '*.*' -exec cp \{\} /Users/admin/Documents/ljworkspace/local/egret/ProStoneAge/ProStoneAge/StoneAppPro/dataBake/ \;
 
 #made  有空格  改一次 弄一次把
-svnPath=/Users/admin/Documents/ljworkspace/local/egret/design/stone_age/Russianstone
+svnPath=/Users/admin/Documents/ljworkspace/local/egret/design/stone_age/Russian_stone
 jsonPath=$svnPath/tools/exceltojson/json
 descPath=$svnPath/tools/exceltojson/ts
 appPath=/Users/admin/Documents/ljworkspace/local/egret/ProStoneAge/ProStoneAge/StoneAppPro/
