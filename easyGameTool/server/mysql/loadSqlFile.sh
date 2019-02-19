04a20237c596626b3b6ab822bf4ea380a023ed93
刘江 <1433727556@qq.com>
2019-02-19 17:48:53 +0800

    add tools

--- easyGameTool/server/mysql/loadSqlFile.sh
+++ easyGameTool/server/mysql/loadSqlFile.sh
@@ -4,7 +4,10 @@
 # 备份  mysqldump -d pkc_webdata playerorderzhenghe -uroot >playerorderzhenghe_t.sql #表 结构
 #mysqldump -t pkc_webdata playerorderzhenghe -uroot >playerorderzhenghe.sql 数据
 
+#dump  表 + 数据  mysqldump -h10.0.4.46 -ugame -perdossol pkc_log189 create_player_log_2019_02_19 > log.sql
+
+
 #scp -P 2202 pikachuadmin@mini-gm.easygametime.com:/home/pikachuadmin/playerloginzhenghe_t.sql /Users/admin/Desktop/
 #scp -P 2202 pikachuadmin@mini-gm.easygametime.com:/home/pikachuadmin/playerloginzhenghe.sql /Users/admin/Desktop/
-scp -P 2202 pikachuadmin@mini-gm.easygametime.com:/home/pikachuadmin/daily_player_2019_01_25_t.sql /Users/admin/Desktop/
+scp -P 2202 pikachuadmin@mini-gm.easygametime.com:/home/pikachuadmin/log.sql /Users/admin/Desktop/
 scp -P 2202 pikachuadmin@mini-gm.easygametime.com:/home/pikachuadmin/daily_player_2019_01_25.sql /Users/admin/Desktop/
