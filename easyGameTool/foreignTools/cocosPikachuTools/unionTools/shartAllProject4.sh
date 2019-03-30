cbf4fe892cb46c81f219bdcdd55629c20fe01d87
刘江 <1433727556@qq.com>
2019-03-30 15:05:01 +0800

    auto jenkins

--- easyGameTool/foreignTools/cocosPikachuTools/unionTools/shartAllProject4.sh
+++ easyGameTool/foreignTools/cocosPikachuTools/unionTools/shartAllProject4.sh
@@ -2,18 +2,14 @@
 #起动 所有 project flask 项目
 
 echo "开始启动 Jenkins"
-
-/Users/admin/Documents/environment/apache-tomcat/bin/catalina.sh run
+sh /Users/admin/Documents/environment/apache-tomcat/bin/shutdown.sh
+sleep 5
+sh /Users/admin/Documents/environment/apache-tomcat/bin/startup.sh
 echo "Jenkins 起动成功"
-
+# http://192.168.1.214:8080/loginError
 
 
 #jenkens /Users/admin/Documents/environment/apache-tomcat/bin/catalina.sh run
 
-
-
-
-
-
-
+#http://192.168.1.214:8888/jenkins/
 
